cc18eb36e3272a975e4ac134d308cd6136e754da
Erik van Ingen <erikvaningen@gmail.com>
2012-08-22 15:58:07 +0200

    first import

---
+++ cbt-web/src/main/java/org/fao/figis/cbt/client/CbtMessages.java
@@ -0,0 +1,11 @@
+package org.fao.figis.cbt.client;
+
+import com.google.gwt.i18n.client.Messages;
+
+public interface CbtMessages extends Messages {
+
+	String sendButton();
+
+	String nameField();
+
+}
---
+++ cbt-web/src/main/java/org/fao/figis/cbt/service/GreetingServiceAsync.java
@@ -0,0 +1,19 @@
+package org.fao.figis.cbt.service;
+
+import org.fao.figis.cbt.client.GreetingService;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public class GreetingServiceAsync implements GreetingService {
+
+	public String greetServer(String name, AsyncCallback<String> asyncCallback) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String greetServer(String name) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
