7dfd0715a2682453461f1c6c8279362c24c25e68
Erik van Ingen <erikvaningen@gmail.com>
2012-09-22 16:38:18 +0200

    Signed-off-by: Erik van Ingen <erikvaningen@gmail.com>

--- cbt-web/src/main/java/org/fao/figis/cbt/web/client/event/EditContactEventHandler.java
+++
@@ -1,7 +0,0 @@
-package org.fao.figis.cbt.web.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface EditContactEventHandler extends EventHandler {
-  void onEditContact(EditContactEvent event);
-}
